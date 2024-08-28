90df487442fd9ef51521a8b7c94b6fd8b2e5b61d
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-08-28 19:01:30 +0530

    Update 08b.fibchild.c

--- 08b.fibchild.c
+++ 08b.fibchild.c
@@ -1,29 +1,47 @@
-//Include   -lrt -o child   while compiling
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
 
-void main(int argc, char* argv[]){
-	int fd, n;
-	int buffer[25];
-	int *shared_memory;
-	
-	fd = shm_open("chandan",O_RDWR, 0666);
-	
-	shared_memory = (int*)mmap(NULL, 1024, PROT_WRITE, MAP_SHARED, fd, 0);
-	
-	n = atoi(argv[1]);
-	
-	buffer[0] = 0;
-	buffer[1] = 1;
-	for(int i=2; i<n;i++){
-		buffer[i] = buffer[i-1] + buffer[i-2];
-	}
-	for(int i=0;i<n;i++) 
-		shared_memory[i]=buffer[i];
+int main(int argc, char *argv[])
+{
+  int k = 2, n1, n2, n3;
+  void *shmptr;
+  int shm_fd = shm_open("OS", O_CREAT | O_RDWR, 0666);
+  ftruncate(shm_fd, 4096);
+  shmptr = mmap(0, 4096, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+  printf("\nChild Printing:\n");
+  int i = atoi(argv[1]);
+  n3 = n1 = 0;
+  n2 = 1;
+  for (k = 1; k <= i; k++)
+  {
+    n3 = n1 + n2;
+    sprintf(shmptr, "%d ", n1);
+    printf("%d ", n1);
+    shmptr += strlen(shmptr);
+    n1 = n2;
+    n2 = n3;
+  }
+
+  return 0;
 }
+/*
+gcc fib_parent.c -o parent
+ gcc fib_child.c -o fib
+./parent 10
+
+Child Printing:
+0 1 1 2 3 5 8 13 21 34 
+[PARENT] Child process completed
+
+Parent printing:
+0 1 1 2 3 5 8 13 21 34 
+*/
