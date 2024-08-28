f92599eab928e59561f4853102a74d3e10c46057
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-08-28 14:57:44 +0530

    Update 07b.fiboparent.c

--- 07b.fiboparent.c
+++ 07b.fiboparent.c
@@ -1,34 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/shm.h>
-#include <sys/wait.h>
+#include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
 
-void main(){
-	int shmid, n;
-	int *shared_memory;
-	
-	shmid = shmget((key_t)110011, 1024, 0666|IPC_CREAT);
-	shared_memory = (int*)shmat(shmid,NULL,0);
-	
-	printf("Enter n>> ");
-	scanf("%d",&n);
-	char nstr[20];
-	sprintf(nstr, "%d", n);
-	
-	if (fork()==0){
-		execlp("./child", "child", nstr, NULL);
-	}
-	else{
-		wait(NULL);
-		
-		for(int i=0;i<n;i++){
-			printf("%d ",shared_memory[i]);
-		}
-		printf("\n");
-		
-		shmdt(shared_memory);
-	
-	}
+int main(int argc, char *argv[])
+{
+    int i;
+    pid_t pid;
+    const int SIZE = 4096;
+    void *shmptr;
+    int shmid = shmget((key_t)1122, 4096, 0666 | IPC_CREAT);
+    shmptr = shmat(shmid, NULL, 0666);
+
+    if (argc > 1)
+    {
+        sscanf(argv[1], "%d", &i);
+        if (i < 1)
+        {
+            printf("\nWrong input is passed:\n");
+            return 0;
+        }
+    }
+    else
+    {
+        printf("\nN is not passed in the command line\n");
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == 0)
+    {
+        // This is the child process part
+        execlp("./fib", "fib", argv[1], NULL);
+    }
+    else if (pid > 0)
+    {
+        wait(NULL); // will wait till child has finished executing
+        printf("\n[PARENT] Child completed\n");
+        printf("\nParent printing:\n");
+        printf("%s\n", (char *)shmptr);
+        shmdt(shmptr);
+    }
+
+    return 0;
 }
