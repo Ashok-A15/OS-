1010dd5ce38156e8e7dbb922433e0812c681548e
Ashok <139360747+Ashok-A15@users.noreply.github.com>
2024-08-28 14:57:22 +0530

    Update 07b.fibochild.c

--- 07b.fibochild.c
+++ 07b.fibochild.c
@@ -1,27 +1,39 @@
-//Compile this program with arguments -> ( -o child )
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/shm.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/shm.h>
+#include <sys/mman.h>
 
-void main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+    int k = 1, n1, n2, n3;
+    void *shmptr;
+    int shmid = shmget((key_t)1122, 4096, 0666);
+    shmptr = shmat(shmid, NULL, 0666);
+    int i = atoi(argv[1]);
 
-	int shmid, n;
-	int buffer[25];
-	int *shared_memory;
-	n = atoi(argv[1]);
-	
-	shmid = shmget((key_t)110011, 1024, 0666);
-	shared_memory = (int*)shmat(shmid,NULL,0);
-		
-	buffer[0] = 0;
-	buffer[1] = 1;
-	for(int i=2; i<n;i++){
-		buffer[i] = buffer[i-1] + buffer[i-2];
-	}
-	
-	for(int i=0;i<n;i++)
-		shared_memory[i]=buffer[i];
+    printf("\nChild Printing:\n");
+    n3 = n1 = 0;
+    n2 = 1;
+    for (k = 0; k < i; k++)
+    {
+        n3 = n1 + n2;
+        sprintf(shmptr, "%d ", n1);
+        printf("%d ", n1);
+        shmptr += strlen(shmptr);
+        n1 = n2;
+        n2 = n3;
+    }
+
+    shmctl(shmid, IPC_RMID, NULL);
+    return 0;
 }
+
+/*gcc fibonacci_parent.c -o parent
+gcc fibonacci_child.c -o fib
+./parent 10
+*/
